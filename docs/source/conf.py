123af92dbd5a37b481b8b47afbcef2c40cbf6664
Benoit TALLANDIER <btallandier@gmail.com>
2022-03-08 15:43:44 +0100

    Star build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Twake'
+copyright = '2022, Linagora'
+author = 'Linagora'
 
 release = '0.1'
 version = '0.1.0'
